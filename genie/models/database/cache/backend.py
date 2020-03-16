e8c91c54126e66359db43db4f0f137158776c858
igoryossugo <iyossugo7@gmail.com>
2020-03-16 20:20:07 -0300

    remove unused files

--- genie/models/database/cache/backend.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-from ramos.mixins import ThreadSafeCreateMixin
-
-from genie.backends.database.backend import DatabaseBackend
-
-
-class CacheDatabaseBackend(DatabaseBackend):
-    id = 'cache'
-    name = 'Cache database'
